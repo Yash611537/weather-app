35137d4a725c6a9da0b4fe9ea426ca92a50c2e26
SolutionsbyDevHive <yashshah2004@gmail.com>
2024-06-14 22:46:54 +0530

    final Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
+ className="main-div"import React, { useState, useEffect } from 'react';
 import './App.css';
 import { FaLocationCrosshairs } from "react-icons/fa6";
 
 const App = () => {
-  const [city, setCity] = useState('Denver');
+  const [city, setCity] = useState('Mumbai');
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -103,7 +103,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div className="main-div">
       <div className="card main-card">
         <div className="search">
           <input type="text" className="search-bar" placeholder="Search" />
